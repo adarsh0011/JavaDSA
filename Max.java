71423eabaf447407ee438032650ebd78f09a1f96
Adarsh <adarshgupta5002@gmail.com>
2026-03-16 20:05:29 +0530

    Initial Java Programs

---
+++ Max.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Max{
+        public static void main(String[] args){
+                int[] arr = {3, 8, 2, 10, 6};
+                int max = arr[0];
+
+                for(int i = 0; i<arr.length; i++){
+                        if(arr[i] > max){
+                                max = arr[i];
+                        }
+	       	}
+		System.out.println(max);
+	}
+}      
---
+++ reverseArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class reverseArray{
+	public static void main(String[] args){
+		int[] arr = {1, 2, 3, 4};
+
+		int n = arr.length;
+
+		for(int i = 0; i< n/2; i++){
+			int temp = arr[i];
+			arr[i] = arr[n-1-i];
+			arr[n-1-i] = temp;
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}

138f072514530f664da17e24d45a4535df78d63c
Adarsh <adarshgupta5002@gmail.com>
2026-03-19 17:35:36 +0530

    arrays two pointers

---
+++ arrays-twopointer/reverse.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class reverse{
+
+	public static void main(String[] args){
+
+		int[] arr = {12, 234, 53, 54, 6};
+
+		reverse(arr);
+		System.out.println(Arrays.toString(arr));
+
+	}
+	static void reverse(int[] arr){
+
+		int left = 0;
+		int right = arr.length - 1;
+
+		while(left<right){
+			
+			swap(arr, left, right);
+			left ++;
+			right --;
+		}
+	}
+	
+	static void swap(int[] arr, int index1, int index2){
+
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+}
